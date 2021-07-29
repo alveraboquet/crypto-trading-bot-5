41a0091bf46605a6f49ddde1af92a42b2f3eaca6
Dan <48622577+danjlewis@users.noreply.github.com>
2021-07-29 14:26:02 +0100

    Implemented Stochastic scoring function.

--- analysis.js
+++ analysis.js
@@ -32,8 +32,46 @@ function calculateEMA(data, numPeriods, targetIndex, smoothing = 2, source = 'cl
     return result;
 }
 
+function calculateStochasticK(data, numPeriods, targetIndex, source = 'close') {
+    targetIndex = targetIndex ?? data.length - 1;
+
+    if (numPeriods <= 0) throw new RangeError('Num. periods parameter out of range!');
+    if (targetIndex >= data.length || targetIndex < 0) throw new RangeError('Target index out of range!');
+    if (targetIndex - numPeriods + 1 < 0) throw new RangeError('Not enough data provided for the number of periods specified!');
+    if (!['open', 'high', 'low', 'close'].includes(source)) throw new Error(`Source parameter (${JSON.stringify(source)}) is invalid!`);
+
+    const dataSlice = data.slice(targetIndex - numPeriods + 1, targetIndex + 1);
+
+    const high = Math.max(...dataSlice.map((period) => period.ohlc.high));
+    const low = Math.min(...dataSlice.map((period) => period.ohlc.low));
+    const currentPrice = dataSlice[dataSlice.length - 1][source];
+    const result = ((currentPrice - low) / (high - low)) * 100;
+
+    return result;
+}
+
+function calculateStochasticD(data, numKPeriods, numDPeriods, targetIndex) {
+    targetIndex = targetIndex ?? data.length - 1;
+
+    if (numPeriods <= 0) throw new RangeError('Num. periods parameter out of range!');
+    if (targetIndex >= data.length || targetIndex < 0) throw new RangeError('Target index out of range!');
+    if (targetIndex - (numKPeriods + numDPeriods) + 1 < 0) throw new RangeError('Not enough data provided for the number of periods specified!');
+
+    const kSlice = [];
+    for (let i = targetIndex; i > targetIndex - numDPeriods; i++) {
+        kSlice.push(calculateStochasticK(data, i, numKPeriods));
+    }
+
+    const sum = kSlice.reduce((p, c) => p + c, 0);
+    const mean = sum / kSlice.length;
+
+    return mean;
+}
+
 // Score functions
 function emaScore(data, targetIndex, numPeriods, smoothing = 2, source = 'close') {
+    targetIndex = targetIndex ?? data.length - 1;
+
     if (numPeriods.some((x) => x <= 0)) throw new RangeError('Num. periods parameter out of range!');
     if (targetIndex >= data.length || targetIndex < 1) throw new RangeError('Target index out of range!');
     if (numPeriods.some((x) => targetIndex - (x * 2) < 0)) throw new RangeError('Not enough data provided for the number of periods specified!');
@@ -59,11 +97,49 @@ function emaScore(data, targetIndex, numPeriods, smoothing = 2, source = 'close'
     return score;
 }
 
+function stochasticScore(data, targetIndex, numKPeriods, numDPeriods, overboughtLevel, oversoldLevel, source = 'close') {
+    targetIndex = targetIndex ?? data.length - 1;
+
+    if (numKPeriods <= 0) throw new RangeError('Num. %K periods parameter out of range!');
+    if (numDPeriods <= 0) throw new RangeError('Num. %D periods parameter out of range!');
+    if (targetIndex >= data.length || targetIndex < 1) throw new RangeError('Target index out of range!');
+    if (targetIndex - (numKPeriods + numDPeriods) < 0) throw new RangeError('Not enough data provided for the number of periods specified!');
+    if (!['open', 'high', 'low', 'close'].includes(source)) throw new Error(`Source parameter (${JSON.stringify(source)}) is invalid!`);
+
+    const kValues = {
+        previous: calculateStochasticK(data, numKPeriods, targetIndex - 1, source),
+        current: calculateStochasticK(data, numKPeriods, targetIndex, source)
+    };
+
+    const dValues = {
+        previous: calculateStochasticD(data, numKPeriods, numDPeriods, targetIndex - 1),
+        current: calculateStochasticD(data, numKPeriods, numDPeriods, targetIndex)
+    };
+
+    let score = 0;
+
+    if (kValues.previous <= dValues.previous && kValues.current > dValues.current) {
+        score += 0.5;
+    } else if (kValues.previous > dValues.previous && kValues.current <= dValues.current) {
+        score += -0.5;
+    }
+
+    if (kValues.previous <= overboughtLevel && kValues.current > overboughtLevel) {
+        score += 0.5;
+    } else if (kValues.previoous > oversoldLevel && kValues.current <= oversoldLevel) {
+        score += 0.5;
+    }
+
+    return score;
+}
+
 function combineScoreFunctions(functions, analysis) {
     return (data, targetIndex) => {
+        targetIndex = targetIndex ?? data.length - 1;
+
         let total = 0;
-        for (const {functionName, args} of functions) {
-            total += analysis[functionName](data, targetIndex, ...args) * (1 / functions.length);
+        for (const {functionName, args, weight} of functions) {
+            total += analysis[functionName](data, targetIndex, ...args) * weight;
         }
 
         total = Math.round(total * 100) / 100;
@@ -76,5 +152,6 @@ function combineScoreFunctions(functions, analysis) {
 
 module.exports = {
     emaScore,
+    stochasticScore,
     combineScoreFunctions
 };
